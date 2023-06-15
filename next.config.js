d4273ccce7f9ce56006771e67d46cd22b1ed3338
Rahul <sahrahul493@gmail.com>
2023-06-15 23:24:40 +0530

    'first-commit'

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["lh3.googleusercontent.com"],
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
